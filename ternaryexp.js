91606f71e265fdc93ceac9a5f415cea6ddfde4f2
Imran Khan <m_imran_khan@me.com>
2017-06-06 20:50:24 +0200

    first commit

---
+++ ternaryexp.js
@@ -0,0 +1,5 @@
+var eatsPlants = true;
+var eatsAnimals = true;
+
+var category =  eatsPlants ? eatsAnimals ? "omnivore" : "carnivore" : eatsPlants ? "herbivore" : "undefined";
+console.log(category);
