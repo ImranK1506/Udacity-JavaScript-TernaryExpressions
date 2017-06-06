bb7aecda87a1a5895813eb8d665a05fb01b4d830
Imran Khan <m_imran_khan@me.com>
2017-06-06 20:53:48 +0200

    used global undefined

--- ternaryexp.js
+++ ternaryexp.js
@@ -1,5 +1,5 @@
 var eatsPlants = true;
 var eatsAnimals = true;
 
-var category =  eatsPlants ? eatsAnimals ? "omnivore" : "carnivore" : eatsPlants ? "herbivore" : "undefined";
+var category =  eatsPlants ? eatsAnimals ? "omnivore" : "carnivore" : eatsPlants ? "herbivore" : undefined;
 console.log(category);
